69aece897346734c418cc500e4dd01b3022e0689
robtallguy <rkoenig5@my.bcit.ca>
2018-04-22 00:33:06 -0700

    Huge update!  Feel like I'm almost on par with where I was at the start of this last Thursday in class.  BUT WITH FIREBASE!
    I can't send an email just yet, so that slows me down.  But what I can do is tomorrrow,
    I can use robtallguy to write a small php that sends emails.  That should help
    
    My program still takes forever to load, but I put that on my end.  Basically the flow is,
    create account
    request a square
    I approve of said square
    database checks if the square is empty, if not, then adds it.
    
    I want to eventually put the paypal option in.  It would fit between checking if the square is empty, reversing the square, then allowing the customer to pay, then adding the square.  I could put in a check to see if any of the squares are unpaid after a day, and then delete them if they aren't.  Or something.

--- checkPixel.php
+++ checkPixel.php
@@ -14,16 +14,14 @@ require 'vendor/autoload.php';
 
 use Google\Cloud\Firestore\FirestoreClient;
 use Google\Cloud\Storage\StorageClient;
-use Google\Cloud\OsLogin\Common;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-$test = new Common\PosixAccount();
+use Google\Cloud\Firestore\FieldValue;
 $bucketid = 'pixelmeme-4e7cf.appspot.com';
 $firestore = new FirestoreClient(['projectId' => 'pixelmeme-4e7cf']);
 $storage = new StorageClient(['projectId' => 'pixelmeme-4e7cf']);
 
-$addedCoords = $firestore->collection('coords')->document('-coords');
-$coords = $addedCoords->collection('savedcoords')->documents();
+
 
 $url = $_GET['url'];
 $ar = explode("_", $url);
@@ -33,83 +31,96 @@ $reqX = $ar[2];
 $reqY = $ar[3];
 $reqSize = $ar[4];
 $ext = $ar[5];
-$pass = $ar[6];
-
-if($pass === 't'){
-	$fileName = $userid."_".$getTime.".".$ext;
-	$memepicDL = $storage->bucket($bucketid)->object("req-memes/". $fileName)
-	->downloadToFile('/var/www/html/PixelMeme/meme_map/'. $fileName);
-
-	$mememapDL = $storage->bucket($bucketid)->object("meme-map/meme-map.png")
-	->downloadToFile('/var/www/html/PixelMeme/meme_map/meme-map(2).png');
-
-	$memePic = imagecreatefromjpeg('meme_map/'.$fileName);
-	$mememapPic = imagecreatefrompng('meme_map/meme-map(2).png');
-
-	//YOU ARE CURRENTLY TRANSFERING IT OVER, great job working out bud
-
-	$meme_map_check = imagecreatefromstring(file_get_contents('meme_map/meme-map(1).png'));
-
-//takes user's clicked square
-	$grab_width = $_POST['photoWidth'];
-	$grab_height = $_POST['photoHeight'];
-
-//defines it to be a grid
-	$grab_width -= ($grab_width%10);
-	$grab_height -= ($grab_height%10);
-
-//size of the photo once added
-	$added_photo_height = $added_photo_width = $_POST['size'];
+$memepicDLWidth = $ar[6];
+$memepicDLHeight = $ar[7];
+$pass = $ar[8];
+$imgurl = $ar[9];
 
 
-	$whitePixel = true;
-//need to look through each pixel, and see if the area is in use or not.
-//loop through the width
-	for($checkPixelWidth = $grab_width; $checkPixelWidth < $grab_width + $added_photo_width; ++$checkPixelWidth){
+//the filename of the image
+$fileName = $userid . "_" . $getTime . "." . $ext;
 
-		//loop through the height
-		for($checkPixelHeight = $grab_height; $checkPixelHeight < $grab_height + $added_photo_height; ++$checkPixelHeight){
-
-			$rgb = imagecolorat($meme_map_check, $checkPixelWidth, $checkPixelHeight);
-			$colours = imagecolorsforindex($meme_map_check, $rgb);
-			if(!($colours['red'] == 255 and $colours['green'] == 255 and $colours['blue'] == 255 and $colours['alpha'] == 0)){
-				$whitePixel = false;
-				break;
-			}
+if($pass) {
+	//create an array to hold the coords of the possible square
+	$coordArray = [];
+	for($x = 0; $x < $reqSize; ++$x){
+		for($y = 0; $y < $reqSize; $y++){
+			$coordArray[$x * $reqSize + $y] = ($reqX + $x) . "," . ($reqY + $y);
 		}
-		//if any of the pixels are white, then we can just leave the loop checker completely.
-		if($whitePixel === false) {
-			break;
+	}
+	$addedCoords = $firestore->collection('coords')->document('saved-coords')
+		->collection('savedcoords')->documents();
+
+	$notFound = true;
+	//First check to see if the square is already taken or not
+	foreach ($addedCoords as $coord) {
+		if (in_array($coord->id(), $coordArray)) {
+			$storage->bucket($bucketid)->object('req-memes/' . $fileName)->delete();
+			//Implement some kind of email to user saying that spot has been taken
+			$notFound = false;
+			header("Location: reqPixel.html");
+			exit();
 		}
 	}
 
-//make sure we can open the file
-	$link_grab = $_POST['hyperLink'];
-	$arraylink = "./meme_map/links.txt";
-	$file = fopen($arraylink, "a") or die("Unable to open file");
+	if ($notFound === true) {
+		//Download the file to be added
+		$memepicDL = $storage->bucket($bucketid)->object("req-memes/" . $fileName)
+			->downloadToFile('meme_map/' . $fileName);
 
-	/*if the pixel is blank*/
-	if($whitePixel === true) {
-		//Order for input of txt
-		//image width, image height, image size, link
-		fwrite($file, $grab_width. "," .$grab_height. "," .$added_photo_height. "," .$link_grab. "\n");
-		fclose($file);
+		//Download the current map
+		$mememapDL = $storage->bucket($bucketid)->object("meme-map/meme-map.png")
+			->downloadToFile('meme_map/meme-map(2).png');
 
-		//check to make sure that the file is added, and can be opened, then add it to the map
-		imagecopyresampled($meme_map, $photo_added, $grab_width, $grab_height,0, 0, $added_photo_width, $added_photo_height, $pic_info[0], $pic_info[1]);
-		imagepng($meme_map, 'meme_map/meme-map(1).png');
+		$memePic = imagecreatefromjpeg('meme_map/' . $fileName);
+		$mememapPic = imagecreatefrompng('meme_map/meme-map(2).png');
 
-		//quit and return
-		header('Location: reqPixel.html');
-		exit;
-	} else {
+		//defines it to be a grid/square
+		$reqX -= ($reqX % 10);
+		$reqY -= ($reqY % 10);
 
-		//quit and return
-		header('Location: reqPixel.html');
-		exit;
+		$i = 0;
+		foreach ($coordArray as $item) {
+			$coords = explode(',', $coordArray[$i++]);
+//			echo $c[0]."+".$c[1]."<br>";
+			$firestore->collection('coords')->document('saved-coords')
+				->collection('savedcoords')->document($coords[0] . "," . $coords[1])->set([
+					'x' => $coords[0],
+					'y' => $coords[1],
+					'time' => $getTime,
+					'user' => $userid,
+					'file' => $fileName
+				]);
+		}
+		imagecopyresampled($mememapPic, $memePic, $reqX, $reqY, 0, 0, $reqSize, $reqSize, $memepicDLWidth, $memepicDLHeight);
+		imagepng($mememapPic, 'meme_map/meme-map(2).png');
+
+		//Upload the map back to storage
+		$storage->bucket($bucketid)->
+		upload(fopen('meme_map/meme-map(2).png', 'r'), [
+			'name' => "meme-map/meme-map.png"
+		]);
+
+		//Upload the added meme to the perma storage
+		$storage->bucket($bucketid)->
+		upload(fopen('meme_map/' . $fileName, 'r'), [
+			'name' => "added-memes/" . $userid . "_". $getTime . ".".$ext
+		]);
+
+		//update the user to have the added meme
+		$firestore->collection('users')->document($userid)->set([
+			$getTime => [
+				'x' => $reqX,
+				'y' => $reqY,
+				'location' => $imgurl
+			]
+		], ['merge' => true]);
+
+		//delete the downloaded files
+		unlink('meme_map/' . $fileName);
+		unlink('meme_map/meme-map(2).png');
 	}
-
+} else {
+	$storage->bucket($bucketid)->object('req-memes/' . $fileName)->delete();
+	echo "file deleted";
 }
-
-
-//$bucket->object()
--- reqPixel.php
+++ reqPixel.php
@@ -15,10 +15,8 @@ require 'vendor/autoload.php';
 
 use Google\Cloud\Firestore\FirestoreClient;
 use Google\Cloud\Storage\StorageClient;
-use Google\Cloud\OsLogin\Common;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-$test = new Common\PosixAccount();
 $bucketid = 'pixelmeme-4e7cf.appspot.com';
 $firestore = new FirestoreClient(['projectId' => 'pixelmeme-4e7cf']);
 $storage = new StorageClient(['projectId' => 'pixelmeme-4e7cf']);
@@ -43,12 +41,7 @@ $coords = $addedCoords->collection('savedcoords')->documents();
 
 
 
-$coordArray = [];
-for($x = 0; $x < $_POST['size']; ++$x){
-	for($y = 0; $y < $_POST['size']; $y++){
-		$coordArray[$x * $_POST['size'] + $y] = ($grab_width + $x) . "," . ($grab_height + $y);
-	}
-}
+
 
 echo '<br><br><br>';
 
@@ -72,69 +65,47 @@ if($target_file) {
     }
 }
 
-
-//(col)coords -> (doc)addedCoords -> (col)coord -> (doc)x,y
-
-
-
-
-
-
 if($canUploadPhoto === true) {
-	//Loop through all the added coord's, and check if they're taken
-	foreach ($coords as $coord) {
-		if (in_array($coord->id(), $coordArray)) {
-			//FOUND COORD IN USE IN DATABASE
-			//REDIRECT BACK
-			echo "found";
-			header("Location: reqPixel.html");
-		}
-	}
-	for($x = 0; $x < $_POST['size']; $x++){
-		for($y = 0; $y < $_POST['size']; $y++){
-			//COORD NOT FOUND
-			$firestore->collection('coords')->document('request-coords')->
-			collection($userid."_".$time)->document($coordArray[$x*$_POST['size']+$y])->set([
-				'x' => $grab_width + $x,
-				'y' => $grab_height + $y,
-				'time' => $time
-			]);
-		}
-	}
 	$ext = pathinfo($_FILES['imageToUpload']['name'], PATHINFO_EXTENSION);
 	$bucket = $storage->bucket($bucketid);
 	$imageUploadData = $bucket->upload(fopen($_FILES['imageToUpload']['tmp_name'], 'r'), [
 		'name' => "req-memes/" . $userid . "_". $time . ".".$ext
 	]);
+
 	$urlOfImage = $imageUploadData->signedUrl(new \Google\Cloud\Core\Timestamp(new \DateTime('tomorrow')));
 	echo $urlOfImage.'<br><br>';
+	echo $pic_info[0].'<br>'.$pic_info[1].'<br>';
+
+	//The link of what I'd need
+	//userid    time   x   y   sizeOfSquare    imageExtension   ImgWidth   ImgHeight   (1 or 0)   url to image
 
 
-	$mail = new PHPMailer(true);
-	try{
-		$mail -> SMTPDebug = 2;
-		$mail -> isSMTP();
-		$mail -> Host = 'ssl://stmp.gmail.com';
-		$mail -> Port = 587;
-		$mail -> SMTPSecure = 'tls';
-		$mail -> SMTPAuth = true;
-		$mail -> Username = "tallguyking@gmai.com";
-		$mail -> Password = "9Jesus921";
-		$mail -> setFrom('tallguyking@gmail.com');
-		$mail -> addReplyTo('robmkoenig@gmail.com');
-		$mail -> addAddress('tallguyking@gmail.com');
-		$mail -> Subject = 'PHPMailer GMAIL test';
-		$mail -> Body = "test: " . $urlOfImage;
-		$mail -> AltBody = 'This is plain text message body';
-		if(!$mail -> send()){
-			echo "Mailer Error: ". $mail->ErrorInfo;
-		} else {
-			echo 'Message Sent!';
-		}
-	}
-	catch (Exception $e){
-		echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
-	}
+
+//	$mail = new PHPMailer(true);
+//	try{
+//		$mail -> SMTPDebug = 2;
+//		$mail -> isSMTP();
+//		$mail -> Host = 'ssl://stmp.gmail.com';
+//		$mail -> Port = 587;
+//		$mail -> SMTPSecure = 'tls';
+//		$mail -> SMTPAuth = true;
+//		$mail -> Username = "tallguyking@gmai.com";
+//		$mail -> Password = "9Jesus921";
+//		$mail -> setFrom('tallguyking@gmail.com');
+//		$mail -> addReplyTo('robmkoenig@gmail.com');
+//		$mail -> addAddress('tallguyking@gmail.com');
+//		$mail -> Subject = 'PHPMailer GMAIL test';
+//		$mail -> Body = "test: " . $urlOfImage;
+//		$mail -> AltBody = 'This is plain text message body';
+//		if(!$mail -> send()){
+//			echo "Mailer Error: ". $mail->ErrorInfo;
+//		} else {
+//			echo 'Message Sent!';
+//		}
+//	}
+//	catch (Exception $e){
+//		echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+//	}
 
 //	$to = 'tallguyking@gmail.com';
 //	$sub = 'map for you - a meme project';
@@ -154,65 +125,3 @@ if($canUploadPhoto === true) {
 } else {
 	echo "false, no image uploaded";
 }
-
-
-//
-////passing the uploaded image to a new file
-//$photo_added = imagecreatefromjpeg('meme_map/'.$_FILES['imageToUpload']['name']);
-//$meme_map = imagecreatefrompng('meme_map/meme-map(1).png');
-//$meme_map_check = imagecreatefromstring(file_get_contents('meme_map/meme-map(1).png'));
-//
-
-//
-
-//
-
-//
-//
-//$whitePixel = true;
-////need to look through each pixel, and see if the area is in use or not.
-////loop through the width
-//for($checkPixelWidth = $grab_width; $checkPixelWidth < $grab_width + $added_photo_width; ++$checkPixelWidth){
-//
-//    //loop through the height
-//    for($checkPixelHeight = $grab_height; $checkPixelHeight < $grab_height + $added_photo_height; ++$checkPixelHeight){
-//
-//        $rgb = imagecolorat($meme_map_check, $checkPixelWidth, $checkPixelHeight);
-//        $colours = imagecolorsforindex($meme_map_check, $rgb);
-//        if(!($colours['red'] == 255 and $colours['green'] == 255 and $colours['blue'] == 255 and $colours['alpha'] == 0)){
-//            $whitePixel = false;
-//            break;
-//        }
-//    }
-//    //if any of the pixels are white, then we can just leave the loop checker completely.
-//    if($whitePixel === false) {
-//        break;
-//    }
-//}
-//
-////make sure we can open the file
-//$link_grab = $_POST['hyperLink'];
-//$arraylink = "./meme_map/links.txt";
-//$file = fopen($arraylink, "a") or die("Unable to open file");
-
-/*if the pixel is blank*/
-//if($whitePixel === true) {
-//    //Order for input of txt
-//    //image width, image height, image size, link
-//    fwrite($file, $grab_width. "," .$grab_height. "," .$added_photo_height. "," .$link_grab. "\n");
-//    fclose($file);
-//
-//    //check to make sure that the file is added, and can be opened, then add it to the map
-//    imagecopyresampled($meme_map, $photo_added, $grab_width, $grab_height,0, 0, $added_photo_width, $added_photo_height, $pic_info[0], $pic_info[1]);
-//    imagepng($meme_map, 'meme_map/meme-map(1).png');
-//
-//    //quit and return
-//    header('Location: reqPixel.html');
-//    exit;
-//} else {
-//
-//    //quit and return
-//    header('Location: reqPixel.html');
-//    exit;
-//}
-#echo '<img src=meme_map/' . $_FILES['imageToUpload']['name'] . ' alt="hello world">';
